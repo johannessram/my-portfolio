0a0b8f73d515b7ea1452717ae4c9e3fd8b11e794
Johanness Ramiandrisoa <johannessram@gmail.com>
2025-01-27 20:43:18 +0300

    fix[WithCardSection]: refer to self.card_titles instead of unknown project_titles

--- WithCardsSection.py
+++ WithCardsSection.py
@@ -6,15 +6,20 @@ from SimpleSection import SimpleSection
 class WithCardsSection(SimpleSection):
 
     def next_project(self):
-        st.session_state['project_index'] = (st.session_state['project_index'] + 1) % len(project_titles)
+        st.session_state['project_index'] = (st.session_state['project_index'] + 1) % len(self.card_titles)
 
 
     def previous_project(self):
-        st.session_state['project_index'] = (st.session_state['project_index'] - 1) % len(project_titles)
+        st.session_state['project_index'] = (st.session_state['project_index'] - 1) % len(self.card_titles)
 
 
     def __init__(self, section_title:str, content_before:str, card_titles:List[str], card_image_paths:List[str], card_descriptions:List[str]):
         super().__init__(section_title, content_before)
+        assert len(card_titles) == len(card_image_paths) == len(card_descriptions), "Lengths of card titles, image paths and descriptions don't match"
+        self.card_titles = card_titles
+        self.card_image_paths = card_image_paths
+        self.card_descriptions = card_descriptions
+
 
         button_left, card_1, card_2, button_right = st.columns([0.08, 0.42, 0.42, 0.08], vertical_alignment="center")
 
